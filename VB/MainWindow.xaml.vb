c606219494a3c7717d1547adedca350126eea3c6
DevExpressExampleBot <fakeEmail>
2025-03-19 19:33:35 +0400

    autogenerate VB

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,36 +1,31 @@
-﻿Imports System.Windows
 Imports DevExpress.Xpf.PdfViewer
 Imports DevExpress.Xpf.Bars
 
 Namespace PopupMenuShowing
 
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
             InitializeComponent()
-
             ' Load a document.
             viewer.OpenDocument("..\..\Demo.pdf")
         End Sub
 
         Private Sub Viewer_PopupMenuShowing(ByVal d As DependencyObject, ByVal e As PopupMenuShowingEventArgs)
-
             ' Remove the Hand tool item from the page context popup menu.
-            Dim removeHandTool As New RemoveAction()
+            Dim removeHandTool As RemoveAction = New RemoveAction()
             removeHandTool.ElementName = DefaultPdfBarManagerItemNames.HandTool
             e.Actions.Add(removeHandTool)
-
             ' Remove the Select All item from the page context popup menu.
-            Dim removeSelectAll As New RemoveAction()
+            Dim removeSelectAll As RemoveAction = New RemoveAction()
             removeSelectAll.ElementName = DefaultPdfBarManagerItemNames.SelectAll
             e.Actions.Add(removeSelectAll)
-
             ' Insert the "Save As..." item invoking the Save As dialog.
-            Dim barButtonItem As New BarButtonItem()
+            Dim barButtonItem As BarButtonItem = New BarButtonItem()
             barButtonItem.Content = "Save As..."
             barButtonItem.Command = viewer.SaveAsCommand
-            Dim insertBarButtonItem As New InsertAction()
+            Dim insertBarButtonItem As InsertAction = New InsertAction()
             insertBarButtonItem.ContainerName = DefaultPdfBarManagerItemNames.ContextMenu
             insertBarButtonItem.Element = barButtonItem
             e.Actions.Add(insertBarButtonItem)
