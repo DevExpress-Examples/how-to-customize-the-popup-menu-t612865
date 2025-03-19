818cb135e37da6390c173122abf534d127324cfa
DevExpressExampleBot <fakeEmail>
2025-03-19 14:46:23 +0400

    Source auto update [skip ci]

--- VB/PopupMenuShowing/MainWindow.xaml.vb
+++ VB/PopupMenuShowing/MainWindow.xaml.vb
@@ -1,36 +1,32 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports DevExpress.Xpf.PdfViewer
 Imports DevExpress.Xpf.Bars
 
 Namespace PopupMenuShowing
 
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-
+            Me.InitializeComponent()
             ' Load a document.
-            viewer.OpenDocument("..\..\Demo.pdf")
+            Me.viewer.OpenDocument("..\..\Demo.pdf")
         End Sub
 
         Private Sub Viewer_PopupMenuShowing(ByVal d As DependencyObject, ByVal e As PopupMenuShowingEventArgs)
-
             ' Remove the Hand tool item from the page context popup menu.
-            Dim removeHandTool As New RemoveAction()
+            Dim removeHandTool As RemoveAction = New RemoveAction()
             removeHandTool.ElementName = DefaultPdfBarManagerItemNames.HandTool
             e.Actions.Add(removeHandTool)
-
             ' Remove the Select All item from the page context popup menu.
-            Dim removeSelectAll As New RemoveAction()
+            Dim removeSelectAll As RemoveAction = New RemoveAction()
             removeSelectAll.ElementName = DefaultPdfBarManagerItemNames.SelectAll
             e.Actions.Add(removeSelectAll)
-
             ' Insert the "Save As..." item invoking the Save As dialog.
-            Dim barButtonItem As New BarButtonItem()
+            Dim barButtonItem As BarButtonItem = New BarButtonItem()
             barButtonItem.Content = "Save As..."
-            barButtonItem.Command = viewer.SaveAsCommand
-            Dim insertBarButtonItem As New InsertAction()
+            barButtonItem.Command = Me.viewer.SaveAsCommand
+            Dim insertBarButtonItem As InsertAction = New InsertAction()
             insertBarButtonItem.ContainerName = DefaultPdfBarManagerItemNames.ContextMenu
             insertBarButtonItem.Element = barButtonItem
             e.Actions.Add(insertBarButtonItem)
